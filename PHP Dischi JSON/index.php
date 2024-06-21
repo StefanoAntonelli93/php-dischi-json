1b4835998292a070d7ac42314bd63d144922d6d6
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-06-21 20:15:53 +0200

    [UPD] add style.css, bootstrap, main.js, vue to project

---
+++ PHP Dischi JSON/index.php
@@ -0,0 +1,33 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dischi JSON</title>
+    <!-- css -->
+    <link rel="stylesheet" href="./style.css">
+    <!-- bootstrap cdn -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+</head>
+
+<body>
+    <div id="app" v-cloak>
+        <header>
+            <h1>{{ title }}</h1>
+        </header>
+    </div>
+    <!-- vue.js -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!-- main js -->
+    <script type="text/javascript" src="main.js"></script>
+</body>
+
+</html>
