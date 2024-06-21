196bddb04e16b73bc1804b57fda3c9022f6a8f3e
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-06-21 21:23:48 +0200

    [UPD] add mockup array discs and DOM style

--- PHP Dischi JSON/index.php
+++ PHP Dischi JSON/index.php
@@ -14,15 +14,55 @@
     <link rel="stylesheet" href="./style.css">
     <!-- bootstrap cdn -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
 
 </head>
 
-<body>
+<body class="bg-dark">
     <div id="app" v-cloak>
-        <header>
-            <h1>{{ title }}</h1>
+        <header class=" bg-warning vw-100 position-fixed top-0">
+            <div class="container">
+
+                <div class="content text-center p-3 d-flex justify-content-between align-items-center">
+                    <img class="logo" src="./images/logo.png" alt="logo">
+                    <h1 class="fw-semibold">{{ title }}</h1>
+                </div>
+            </div>
         </header>
+        <main class="main">
+            <section>
+                <div class="container bg-warning-subtle">
+
+                    <div class="content">
+                        <ul class="d-flex flex-wrap gap-3 list-unstyled p-5 mt-5">
+                            <li class="p-5 border border-5 border-secondary-subtle" v-for="disc in discs">
+                                <img :src="disc.img" :alt="disc.name">
+                                <div>{{ disc.name }}</div>
+                                <div>{{ disc.artist }}</div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer class="bg-warning">
+            <div class="container">
+                <div class="content p-2">
+                    <div class="row align-items-center justify-content-between">
+                        <div class="col-auto">
+                            <p>PHP dischi JSON</p>
+                        </div>
+                        <div class="col-auto">
+                            <img class="footer-logo" src="./images/logo.png" alt="logo">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </div>
     <!-- vue.js -->
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
--- PHP Dischi JSON/main.js
+++ PHP Dischi JSON/main.js
@@ -4,6 +4,119 @@ createApp({
   data() {
     return {
       title: "PHP Dischi JSON",
+      //   array di mockup
+      discs: [
+        {
+          name: "asdf",
+          artist: 1,
+          cover: "img",
+        },
+        {
+          name: "asdffafaf",
+          artist: 2,
+          cover: "img",
+        },
+        {
+          name: "asdfafafagfagagf",
+          artist: 3,
+          cover: "img",
+        },
+        {
+          name: "asdfagfasa",
+          artist: 4,
+          cover: "img",
+        },
+        {
+          name: "asdfafg",
+          artist: 5,
+          cover: "img",
+        },
+        {
+          name: "asdfafga",
+          artist: 6,
+          cover: "img",
+        },
+        {
+          name: "asdfafga",
+          artist: 6,
+          cover: "img",
+        },
+        {
+          name: "asdfafga",
+          artist: 6,
+          cover: "img",
+        },
+        {
+          name: "asdfafga",
+          artist: 6,
+          cover: "img",
+        },
+        {
+          name: "asdfafga",
+          artist: 6,
+          cover: "img",
+        },
+        {
+          name: "asdfafga",
+          artist: 6,
+          cover: "img",
+        },
+        {
+          name: "asdfafga",
+          artist: 6,
+          cover: "img",
+        },
+        {
+          name: "asdfafga",
+          artist: 6,
+          cover: "img",
+        },
+        {
+          name: "asdfafga",
+          artist: 6,
+          cover: "img",
+        },
+        {
+          name: "asdfafga",
+          artist: 6,
+          cover: "img",
+        },
+        {
+          name: "asdfafga",
+          artist: 6,
+          cover: "img",
+        },
+        {
+          name: "asdfafga",
+          artist: 6,
+          cover: "img",
+        },
+        {
+          name: "asdfafga",
+          artist: 6,
+          cover: "img",
+        },
+        {
+          name: "asdfafga",
+          artist: 6,
+          cover: "img",
+        },
+        {
+          name: "asdfafga",
+          artist: 6,
+          cover: "img",
+        },
+        {
+          name: "asdfafga",
+          artist: 6,
+          cover: "img",
+        },
+        {
+          name: "asdfafga",
+          artist: 6,
+          cover: "img",
+        },
+      ],
     };
   },
 }).mount("#app");
